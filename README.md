e507faef4d254c49320aa3aa3897a1943952f383
gianpc <gpaucarcortez@gmail.com>
2025-03-12 11:18:28 -0500

    feat: se añadio redirect y se hizo el dashboard dinámico

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. Inicio de proyecto dashboard
 2. Estructura de nuestro dashboard
 3. Sidebar y contenido principal
+4. Next/Image
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,8 @@ import { redirect } from "next/navigation";
 
 export default function HomePage() {
 
-  redirect("/dashboard/counter");
-  return (
-    <>
-      <h1>
-        Hola Mundo
-      </h1>
-    </>
-  );
+  redirect("/dashboard/main");
+
 }
+
+
